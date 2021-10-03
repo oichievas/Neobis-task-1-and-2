b047637319e3a38067631406f73cc3baafa8853f
Saule <sauleojcieva225@gmail.com>
2021-10-03 18:33:22 +0600

    add taks about mohth

--- script.js
+++ script.js
@@ -130,3 +130,14 @@ if (ask_smile) {
 }
 
 // End Smile
+
+// get the month name from a particular date
+
+var month_name = function(dt){
+  let month_list=[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    return month_list[dt.getMonth()];
+  };
+  console.log(month_name(new Date("10/11/2009")));
+  console.log(month_name(new Date("11/13/2014")));
+
+// End get the month name from a particular date
