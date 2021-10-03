13b8fdf333f2d20455e5920564b4d4880c9ffd42
Saule <sauleojcieva225@gmail.com>
2021-10-03 18:36:55 +0600

    some changes

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ var month_name = function(dt){
   let month_list=[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
     return month_list[dt.getMonth()];
   };
-  console.log(month_name(new Date("10/11/2009")));
-  console.log(month_name(new Date("11/13/2014")));
+  console.log(month_name(new Date("01/18/2021")));
+  console.log(month_name(new Date("05/24/2022")));
 
 // End get the month name from a particular date
